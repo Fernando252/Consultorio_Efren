7d33499a5315f0fab940bac1e1e5843d118133a6
Fernando252 <50157177+Fernando252@users.noreply.github.com>
2024-02-18 16:43:12 -0500

    Update settings.py

--- holamundo/consultorio/settings.py
+++ holamundo/consultorio/settings.py
@@ -151,7 +151,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Configuración de archivos estáticos
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'consultorio\static'),
+    os.path.join(BASE_DIR, 'static'),
  
 ]
 
